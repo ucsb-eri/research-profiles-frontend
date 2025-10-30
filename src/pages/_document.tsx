98fb239efed8af59bb2ea904973a875b83728be9
Brian Kim <156366811+ssebango2@users.noreply.github.com>
2025-10-30 12:26:07 -0700

    fixing viewport issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://webfonts.brand.ucsb.edu/webfont.min.css" rel="stylesheet" />
       </Head>
       <body>
